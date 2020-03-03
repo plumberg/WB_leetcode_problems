30a2564c42edfe72febf78c295611f0d4855355f
Plumberg <hana.ryzhuk@gmail.com>
2020-03-03 17:10:27 -0500

    Queue problems solved

---
+++ WB_Leetcode_4/Implement Queue using Stacks.java
@@ -0,0 +1,47 @@
+class MyQueue {
+    Stack<Integer>stack;
+    Stack<Integer>stack1;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack = new Stack<>();
+        stack1 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while(!stack1.isEmpty()){
+            stack.push(stack1.pop());
+        }
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        while(!stack.isEmpty()){
+            stack1.push(stack.pop());
+        }
+        return stack1.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+
+        while(!stack.isEmpty()){
+            stack1.push(stack.pop());
+        }
+        return stack1.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.isEmpty()&&stack1.isEmpty();
+    }
+}
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
